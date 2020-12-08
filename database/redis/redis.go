524ce0045f7b3133ced942d1b3c9a201787bdaeb
Emad <emadghaffariii@gmail.com>
2020-12-08 12:13:46 +0000

    [refactor] cleanup redis system

--- database/redis/redis.go
+++ database/redis/redis.go
@@ -1,8 +1,11 @@
 package redis
 
 import (
+	"context"
 	"fmt"
 	"sync"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/spf13/viper"
@@ -12,30 +15,36 @@ var (
 	once sync.Once
 
 	// DB variable
-	DB iredis = &sredis{}
+	DB rs = &redi{}
 )
 
-type iredis interface {
+type rs interface {
 	New()
 	GetDB() *redis.Client
 }
 
-type sredis struct {
+type redi struct {
 	db *redis.Client
 }
 
-func (s *sredis) New() {
+func (s *redi) New() {
 	once.Do(func() {
 		s.db = redis.NewClient(&redis.Options{
-			Addr:     viper.GetString("redis.Addr"),
-			Password: viper.GetString("redis.Password"), // no password set
-			DB:       viper.GetInt("redis.DB"),          // use default DB
+			Addr: viper.GetString("redis.Addr"),
+			// Password: viper.GetString("redis.Password"), // no password set
+			DB: viper.GetInt("redis.DB"), // use default DB
 		})
+
+		if err := s.db.Ping(context.Background()).Err(); err != nil {
+			log.WithFields(log.Fields{
+				"error": fmt.Sprintf("Config redis database Error: %s", err),
+			}).Fatal(fmt.Sprintf("Config redis database Error: %s", err))
+		}
 
 		fmt.Println("redis connected")
 	})
 }
 
-func (s *sredis) GetDB() *redis.Client {
+func (s *redi) GetDB() *redis.Client {
 	return s.db
 }
