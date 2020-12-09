081c6db57b6168bbd009549579c87cde1fe640bc
Emad <emadghaffariii@gmail.com>
2020-12-09 15:49:50 +0000

    [feature] model for user login and register, add validations

--- model/user/user_dao.go
+++ model/user/user_dao.go
@@ -1,15 +1,66 @@
 package user
 
 import (
+	"fmt"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/emadghaffari/api-teacher/database/postgres"
+	"github.com/emadghaffari/api-teacher/utils/date"
 	"github.com/emadghaffari/res_errors/errors"
+	log "github.com/sirupsen/logrus"
 )
+
+var (
+	loginQuery = "SELECT cp.id,cp.name,cp.lname,cp.identitiy,m.name as role FROM roles as m INNER JOIN user_roles as p ON m.id = p.role_id INNER JOIN users as cp ON p.user_id = cp.id WHERE cp.identitiy = $1 AND cp.password = $2 ;"
+)
+
+// SELECT cp.id,cp.name,cp.lname,cp.identitiy,m.name FROM users WHERE identitiy= $1 AND password= $2 ;
 
 // Register meth create new User
 func (c *User) Register() errors.ResError {
+	db := postgres.DB.GetDB()
+
+	query, args, err := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar).
+		Insert("users").Columns("name", " lname", " identitiy", " password", " created_at").
+		Values(c.FirstName, c.LastName, c.Identitiy, c.Password, date.GetNowString()).
+		Suffix("RETURNING id").ToSql()
+
+	if err != nil {
+		log.Error(fmt.Sprintf("Error in StatementBuilder: %s", err))
+		return errors.HandlerInternalServerError(err.Error(), err)
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		log.Error(fmt.Sprintf("Error in BeginDB: %s", err))
+		return errors.HandlerInternalServerError(err.Error(), err)
+	}
+
+	if err := tx.QueryRow(query, args...).Scan(&c.ID); err != nil {
+		log.Error(fmt.Sprintf("Error in QueryRow: %s", err))
+		return errors.HandlerInternalServerError(err.Error(), err)
+	}
+	tx.Commit()
+
 	return nil
 }
 
 // Login meth for Login Users
 func (c *User) Login() errors.ResError {
+	db := postgres.DB.GetDB()
+	stms, err := db.Prepare(loginQuery)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error in Select Role: %s", err))
+
+		return errors.HandlerInternalServerError(err.Error(), err)
+	}
+	defer stms.Close()
+	result := stms.QueryRow(c.Identitiy, c.Password)
+
+	if err := result.Scan(&c.ID, &c.FirstName, &c.LastName, &c.Identitiy, &c.Role.Name); err != nil {
+		log.Error(fmt.Sprintf("Error in Select Role: %s", err))
+
+		return errors.HandlerInternalServerError(err.Error(), err)
+	}
+
 	return nil
 }
--- model/user/user_dto.go
+++ model/user/user_dto.go
@@ -1,5 +1,12 @@
 package user
 
+import (
+	"strings"
+
+	"github.com/emadghaffari/api-teacher/model/role"
+	"github.com/emadghaffari/res_errors/errors"
+)
+
 var (
 	// Model for User
 	Model user = &User{}
@@ -9,17 +16,42 @@ type user interface{}
 
 // User struct
 type User struct {
-	ID        int64  `json:"id"`
-	FirstName string `json:"name"`
-	LastName  string `json:"lname"`
-	Identitiy string `json:"identitiy"`
-	CreatedAt string `json:"created_at"`
-	Password  string `json:"_"`
-	Role      Role
+	ID        int64     `json:"-"`
+	FirstName string    `json:"name"`
+	LastName  string    `json:"lname"`
+	Identitiy string    `json:"identitiy"`
+	CreatedAt string    `json:"created_at"`
+	Password  string    `json:"password"`
+	Role      role.Role `json:"role"`
 }
 
-// Roles: teacher, student
-type Role struct {
-	RoleID int64  `json:"id"`
-	Name   string `json:"name"`
+// RegisterValidate user
+func (us *User) RegisterValidate() errors.ResError {
+	if us.FirstName == "" {
+		return errors.HandlerBadRequest("FirstName is Empty.")
+	}
+	if us.LastName == "" {
+		return errors.HandlerBadRequest("LastName is Empty.")
+	}
+	if us.Password == "" {
+		return errors.HandlerBadRequest("Password is Empty.")
+	}
+	if us.Role.Name == "" {
+		return errors.HandlerBadRequest("Role is Empty.")
+	}
+	return nil
+}
+
+// LoginValidate user
+func (us *User) LoginValidate() errors.ResError {
+	us.Password = strings.TrimSpace(us.Password)
+	if us.Password == "" {
+		return errors.HandlerBadRequest("Invalid Password.")
+	}
+
+	if us.Identitiy == "" {
+		return errors.HandlerBadRequest("Invalid Identitiy.")
+	}
+
+	return nil
 }
