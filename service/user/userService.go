f32a35f2f83fd723d5141502cacd3061f6f92f51
Emad <emadghaffariii@gmail.com>
2020-12-09 15:50:28 +0000

    [feature] add login and register

--- service/user/userService.go
+++ service/user/userService.go
@@ -1,9 +1,13 @@
 package user
 
 import (
+	"fmt"
+
 	"github.com/emadghaffari/api-teacher/config/token"
+	"github.com/emadghaffari/api-teacher/model/role"
 	model "github.com/emadghaffari/api-teacher/model/user"
 	"github.com/emadghaffari/res_errors/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -19,19 +23,35 @@ type users interface {
 type user struct{}
 
 func (usr *user) Login(us model.User) (*token.TokenDetails, errors.ResError) {
-	err := us.Login()
-	if err != nil {
+	if err := us.Login(); err != nil {
 		return nil, err
 	}
+
 	ts, _ := token.Conf.Generate(us)
 	return ts, nil
 }
 
 func (usr *user) Register(us model.User) (*token.TokenDetails, errors.ResError) {
-	err := us.Register()
-	if err != nil {
+	rl := role.Role{Name: us.Role.Name}
+	if err := rl.GetByName(); err != nil {
+		log.Error(fmt.Sprintf("Role Not Found: %s", err))
+
 		return nil, err
 	}
+
+	if err := us.Register(); err != nil {
+		return nil, err
+	}
+
+	if err := rl.Assign(us.ID); err != nil {
+		log.WithFields(log.Fields{
+			"user_id": us.ID,
+			"role_id": rl.ID,
+		}).Error(fmt.Sprintf("Faild Assign Role To User: %s", err))
+		return nil, err
+	}
+
+	us.Role.Name = rl.Name
 	ts, _ := token.Conf.Generate(us)
 	return ts, nil
 }
