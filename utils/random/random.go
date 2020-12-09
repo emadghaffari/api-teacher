f38175d256dc1a6666e96a5c373ade8e088216fc
Emad <emadghaffariii@gmail.com>
2020-12-09 15:48:35 +0000

    [feature] add random package

---
+++ utils/random/random.go
@@ -0,0 +1,33 @@
+package random
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Slice random Slice of numbers
+func Slice(num, min, max int) []int {
+	if min > max {
+		fmt.Println("Error min and max")
+		return nil
+	}
+	rand.Seed(time.Now().UnixNano())
+	var items []int
+	for i := 0; i < num; i++ {
+		random := (rand.Intn((max - min)) + min)
+		items = append(items, random)
+	}
+	return items
+}
+
+// Rand between numbers
+func Rand(min, max int) int {
+	if min > max {
+		fmt.Println("Error min and max")
+		return 0
+	}
+	rand.Seed(time.Now().UnixNano())
+	random := (rand.Intn((max - min)) + min)
+	return random
+}
