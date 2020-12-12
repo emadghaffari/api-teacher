a1cbdf463ccf45f100549a67e15d5ef99904b80c
Emad <emadghaffariii@gmail.com>
2020-12-12 15:19:56 +0000

    [feature] add index meth, for get list of courses - service

--- service/course/courseService.go
+++ service/course/courseService.go
@@ -1,10 +1,32 @@
 package course
 
+import (
+	"time"
+
+	"github.com/emadghaffari/api-teacher/database/redis"
+	model "github.com/emadghaffari/api-teacher/model/course"
+	"github.com/emadghaffari/res_errors/errors"
+)
+
 var (
 	// Service var
 	Service courses = &course{}
 )
 
-type courses interface{}
+type courses interface {
+	Index() ([]*model.Course, errors.ResError)
+}
 
 type course struct{}
+
+func (cs *course) Index() (items []*model.Course, err errors.ResError) {
+	redis.DB.Get("courses", &items)
+	if len(items) == 0 {
+		items, err = model.Model.Index()
+		if err != nil {
+			return nil, err
+		}
+		redis.DB.Set("courses", items, time.Duration(time.Hour*72))
+	}
+	return items, nil
+}
