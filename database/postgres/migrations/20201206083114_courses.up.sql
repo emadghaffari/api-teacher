95ceffe69c5898df36dc2d384956b1b7317acae7
Emad <emadghaffariii@gmail.com>
2020-12-13 11:58:24 +0000

    [hot-fix] fix database and seeder for postgres

--- database/postgres/migrations/20201206083114_courses.up.sql
+++ database/postgres/migrations/20201206083114_courses.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE courses (
       REFERENCES users (id),
 	name VARCHAR ( 50 ) NOT NULL, -- course name
 	identitiy VARCHAR ( 50 ) UNIQUE NOT NULL, -- identitiy of course
-    valence SMALLINT DEFAULT 25 NOT NULL CHECK (valence > 0), -- Capacity for each course, default is 25
+    valence SMALLINT DEFAULT 25 NOT NULL CHECK (valence >= 0), -- Capacity for each course, default is 25
     time VARCHAR ( 150 ) NOT NULL, -- course time and class detail 
     created_at TIMESTAMP NOT NULL
 );
