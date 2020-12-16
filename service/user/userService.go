d5962837f4235b484d47e9db9ed055d520f2fdcc
Emad <emadghaffariii@gmail.com>
2020-12-16 15:39:33 +0000

    [refactor] cleanup user service

--- service/user/userService.go
+++ service/user/userService.go
@@ -1,13 +1,11 @@
 package user
 
 import (
-	"fmt"
-
-	"github.com/emadghaffari/api-teacher/config/token"
+	jst "github.com/emadghaffari/api-teacher/config/token"
 	"github.com/emadghaffari/api-teacher/model/role"
 	model "github.com/emadghaffari/api-teacher/model/user"
+	"github.com/emadghaffari/api-teacher/service/token"
 	"github.com/emadghaffari/res_errors/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -16,46 +14,31 @@ var (
 )
 
 type users interface {
-	Login(model.User) (*token.TokenDetails, errors.ResError)
-	Register(model.User) (*token.TokenDetails, errors.ResError)
+	Login() (*jst.TokenDetails, errors.ResError)
+	Register() (*jst.TokenDetails, errors.ResError)
 }
 
 type user struct{}
 
-func (usr *user) Login(us model.User) (*token.TokenDetails, errors.ResError) {
-	if err := us.Login(); err != nil {
+func (usr *user) Login() (*jst.TokenDetails, errors.ResError) {
+	if err := model.Model.Login(); err != nil {
 		return nil, err
 	}
 
-	ts, _ := token.Conf.Generate(us)
-	return ts, nil
+	return token.Service.Generate(*model.Model.Get())
 }
 
-func (usr *user) Register(us model.User) (*token.TokenDetails, errors.ResError) {
-	rl := role.Role{Name: us.Role.Name}
-	if err := rl.GetByName(); err != nil {
-		log.Error(fmt.Sprintf("Role Not Found: %s", err))
-
+func (usr *user) Register() (*jst.TokenDetails, errors.ResError) {
+	if err := model.Model.Register(); err != nil {
 		return nil, err
 	}
 
-	if err := us.Register(); err != nil {
+	us := model.Model.Get()
+	if err := role.Model.Assign(us.ID); err != nil {
 		return nil, err
 	}
 
-	if err := rl.Assign(us.ID); err != nil {
-		log.WithFields(log.Fields{
-			"user_id": us.ID,
-			"role_id": rl.ID,
-		}).Error(fmt.Sprintf("Faild Assign Role To User: %s", err))
-		return nil, err
-	}
+	us.Role = &role.Role{ID: role.Model.Get().ID, Name: role.Model.Get().Name}
 
-	us.Role.Name = rl.Name
-	ts, err := token.Conf.Generate(us)
-	if err != nil {
-		return nil, errors.HandlerInternalServerError("Error in Generate Token: %s", err)
-	}
-
-	return ts, nil
+	return token.Service.Generate(*us)
 }
--- service/user/userService_test.go
+++ service/user/userService_test.go
@@ -1,16 +1,17 @@
 package user
 
 import (
-	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/emadghaffari/api-teacher/config/token"
+	"github.com/emadghaffari/api-teacher/model/role"
 	model "github.com/emadghaffari/api-teacher/model/user"
+	srv "github.com/emadghaffari/api-teacher/service/token"
 	"github.com/emadghaffari/res_errors/errors"
 	"github.com/stretchr/testify/assert"
 )
 
+// mock for user model
 type mockModel struct {
 	MockFuc func() errors.ResError
 }
@@ -21,59 +22,148 @@ func (mc *mockModel) Register() errors.ResError {
 func (mc *mockModel) Login() errors.ResError {
 	return mc.MockFuc()
 }
-func (mc *mockModel) Set(m *model.User) {}
-func (mc *mockModel) Get() *model.User  { return nil }
+func (mc *mockModel) Set(m *model.User)                 {}
+func (mc *mockModel) Get() *model.User                  { return &model.User{} }
+func (mc *mockModel) RegisterValidate() errors.ResError { return nil }
+func (mc *mockModel) LoginValidate() errors.ResError    { return nil }
 
+// mock for token
 type mockToken struct {
-	GenMockFuc func() (*token.TokenDetails, error)
-	VerMockFuc func() (*token.AccessDetails, error)
+	GenMockFuc func() (*token.TokenDetails, errors.ResError)
 }
 
-func (mc *mockToken) Generate(user model.User) (*token.TokenDetails, error) {
+func (mc *mockToken) Generate(model.User) (*token.TokenDetails, errors.ResError) {
 	return mc.GenMockFuc()
 }
-func (mc *mockToken) VerifyToken(string) (*token.AccessDetails, error) {
-	return mc.VerMockFuc()
+
+// mock for role
+type mockRole struct {
+	MockFuc func() *role.Role
+	MockErr func() errors.ResError
 }
 
-func TestLogin(t *testing.T) {
+func (mc *mockRole) Set(rl *role.Role)               {}
+func (mc *mockRole) Get() *role.Role                 { return mc.MockFuc() }
+func (mc *mockRole) Insert() errors.ResError         { return mc.MockErr() }
+func (mc *mockRole) GetByName() errors.ResError      { return mc.MockErr() }
+func (mc *mockRole) Assign(id int64) errors.ResError { return mc.MockErr() }
+
+func TestRegister(t *testing.T) {
+	// mock user model AND tokenService
 	modelMocked := mockModel{}
 	tokenMocked := mockToken{}
+	roleMocked := mockRole{}
 	tests := []struct {
-		step       string
-		status     int
-		error      string
-		param      model.User
-		modelMock  func() errors.ResError
-		genMockFuc func() (*token.TokenDetails, error)
-		verMockFuc func() (*token.AccessDetails, error)
+		step        string
+		status      int
+		error       string
+		MockFuc     func() errors.ResError
+		genMockFuc  func() (*token.TokenDetails, errors.ResError)
+		roleMockFuc func() *role.Role
+		MockErr     func() errors.ResError
 	}{
 		{
-			step:       "a",
-			status:     http.StatusOK,
-			error:      "",
-			param:      model.User{},
-			modelMock:  func() errors.ResError { return nil },
-			genMockFuc: func() (*token.TokenDetails, error) { return nil, nil },
-			verMockFuc: func() (*token.AccessDetails, error) { return nil, nil },
+			step:        "a",
+			error:       "",
+			MockFuc:     func() errors.ResError { return nil },
+			genMockFuc:  func() (*token.TokenDetails, errors.ResError) { return &token.TokenDetails{}, nil },
+			roleMockFuc: func() *role.Role { return &role.Role{ID: 1, Name: ""} },
+			MockErr:     func() errors.ResError { return nil },
+		},
+		{
+			step:        "b",
+			error:       "BAD REQUEST",
+			MockFuc:     func() errors.ResError { return errors.HandlerBadRequest("BAD REQUEST") },
+			genMockFuc:  func() (*token.TokenDetails, errors.ResError) { return &token.TokenDetails{}, nil },
+			roleMockFuc: func() *role.Role { return &role.Role{ID: 1, Name: ""} },
+			MockErr:     func() errors.ResError { return nil },
+		},
+		{
+			step:        "c",
+			error:       "BAD REQUEST",
+			MockFuc:     func() errors.ResError { return nil },
+			genMockFuc:  func() (*token.TokenDetails, errors.ResError) { return &token.TokenDetails{}, nil },
+			roleMockFuc: func() *role.Role { return &role.Role{ID: 0, Name: ""} },
+			MockErr:     func() errors.ResError { return errors.HandlerBadRequest("BAD REQUEST") },
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.step, func(t *testing.T) {
-			// model mock
-			modelMocked.MockFuc = tc.modelMock
+
+			modelMocked.MockFuc = tc.MockFuc
 			model.Model = &modelMocked
 
-			// token mock
+			// mock token generate
 			tokenMocked.GenMockFuc = tc.genMockFuc
-			tokenMocked.VerMockFuc = tc.verMockFuc
+			srv.Service = &tokenMocked
 
-			tdt, err := Service.Login(tc.param)
-			if err != nil {
-				assert.Equal(t, "", "")
+			// mock role
+			roleMocked.MockFuc = tc.roleMockFuc
+			roleMocked.MockErr = tc.MockErr
+			role.Model = &roleMocked
+
+			Service.Register()
+
+		})
+	}
+}
+
+func TestLogin(t *testing.T) {
+	// mock user model AND tokenService
+	modelMocked := mockModel{}
+	tokenMocked := mockToken{}
+
+	tests := []struct {
+		step       string
+		status     int
+		error      string
+		param      model.User
+		MockFuc    func() errors.ResError
+		genMockFuc func() (*token.TokenDetails, errors.ResError)
+	}{
+		{
+			step:       "a",
+			error:      "",
+			param:      model.User{},
+			MockFuc:    func() errors.ResError { return nil },
+			genMockFuc: func() (*token.TokenDetails, errors.ResError) { return &token.TokenDetails{}, nil },
+		},
+		{
+			step:       "a",
+			error:      "BAD REQUEST",
+			param:      model.User{},
+			MockFuc:    func() errors.ResError { return errors.HandlerBadRequest("BAD REQUEST") },
+			genMockFuc: func() (*token.TokenDetails, errors.ResError) { return &token.TokenDetails{}, nil },
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.step, func(t *testing.T) {
+
+			modelMocked.MockFuc = tc.MockFuc
+			model.Model = &modelMocked
+
+			// mock token generate
+			tokenMocked.GenMockFuc = tc.genMockFuc
+			srv.Service = &tokenMocked
+
+			jwt, err := Service.Login()
+			if err == nil {
+				if tc.error == "" {
+					assert.Equal(t, jwt.AccessToken, "")
+				} else {
+					assert.Fail(t, err.Error())
+				}
 			}
-			fmt.Println(tdt)
+			if err != nil {
+				if tc.error != "" {
+					assert.Equal(t, err.Message(), tc.error)
+				} else {
+					assert.Fail(t, err.Message())
+				}
+			}
+
 		})
 	}
 }
